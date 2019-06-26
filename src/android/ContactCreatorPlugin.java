af2b9b491bb6465a7eb8fe97ae71abfcb830e8b0
Joerg Debus <joerg.debus@we-do.ai>
2019-06-26 16:12:02 +0200

    Show account choose intent and create contact in background without intent

--- src/android/ContactCreatorPlugin.java
+++ src/android/ContactCreatorPlugin.java
@@ -1,6 +1,8 @@
 package nl.afas.cordova.plugin.contactcreator;
 
+import android.accounts.Account;
 import android.app.Activity;
+import android.content.ContentProviderOperation;
 import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
@@ -12,6 +14,7 @@ import android.provider.ContactsContract.Intents;
 import android.provider.ContactsContract.PhoneLookup;
 import android.util.Log;
 import android.widget.Toast;
+import android.accounts.AccountManager;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -26,6 +29,9 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+
+import static android.app.Activity.RESULT_OK;
 
 public class ContactCreatorPlugin extends CordovaPlugin {
 
@@ -34,78 +40,28 @@ public class ContactCreatorPlugin extends CordovaPlugin {
   private static final int CHOOSE_CONTACT = 1;
   private static final int INSERT_CONTACT = 2;
 
+  private static JSONObject contact;
+
   @Override
   public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
     this.callbackContext = callbackContext;
     this.context = cordova.getActivity().getApplicationContext();
 
+
     if (action.equals("addContact")) {
       int phone_count = 0;
       int email_count = 0;
       Intent intent = new Intent(Intent.ACTION_INSERT);
-      intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
+      intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+
       try {
-        JSONObject contact = data.getJSONObject(0);
+        contact = data.getJSONObject(0);
         if (contact != null) {
 
-          intent.putExtra(ContactsContract.Intents.Insert.NAME, contact.optString("displayName"));
+          Intent pickIntent = AccountManager.newChooseAccountIntent(null, null, new String[] {"com.microsoft.office.outlook.USER_ACCOUNT", "com.google"}, true, null, null, null, null);
+          cordova.startActivityForResult(this, pickIntent, 1);
 
-
-          if (!contact.getString("phoneHome").equals("")) {
-            intent.putExtra(ContactsContract.Intents.Insert.PHONE_TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_HOME);
-            intent.putExtra(ContactsContract.Intents.Insert.PHONE, contact.getString("phoneHome"));
-            phone_count++;
-          }
-
-          if (!contact.getString("phoneWork").equals("")) {
-            if (phone_count == 0) {
-              intent.putExtra(ContactsContract.Intents.Insert.PHONE_TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_WORK);
-              intent.putExtra(ContactsContract.Intents.Insert.PHONE, contact.getString("phoneWork"));
-              phone_count++;
-            } else if (phone_count == 1) {
-              intent.putExtra(ContactsContract.Intents.Insert.SECONDARY_PHONE_TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_WORK);
-              intent.putExtra(ContactsContract.Intents.Insert.SECONDARY_PHONE, contact.getString("phoneWork"));
-              phone_count++;
-            }
-          }
-
-          if (!contact.getString("mobileHome").equals("")) {
-            if (phone_count == 0) {
-              intent.putExtra(ContactsContract.Intents.Insert.PHONE_TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE);
-              intent.putExtra(ContactsContract.Intents.Insert.PHONE, contact.getString("mobileHome"));
-            } else if (phone_count == 1) {
-              intent.putExtra(ContactsContract.Intents.Insert.SECONDARY_PHONE_TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE);
-              intent.putExtra(ContactsContract.Intents.Insert.SECONDARY_PHONE, contact.getString("mobileHome"));
-            } else if (phone_count == 2) {
-              intent.putExtra(ContactsContract.Intents.Insert.TERTIARY_PHONE_TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE);
-              intent.putExtra(ContactsContract.Intents.Insert.TERTIARY_PHONE, contact.getString("mobileHome"));
-            }
-          }
-
-          if (!contact.getString("emailHome").equals("")) {
-            intent.putExtra(ContactsContract.Intents.Insert.EMAIL_TYPE, ContactsContract.CommonDataKinds.Email.TYPE_HOME);
-            intent.putExtra(ContactsContract.Intents.Insert.EMAIL, contact.getString("emailHome"));
-            email_count++;
-          }
-
-          if (!contact.getString("emailWork").equals("")) {
-            if (email_count == 0) {
-              intent.putExtra(ContactsContract.Intents.Insert.EMAIL_TYPE, ContactsContract.CommonDataKinds.Email.TYPE_WORK);
-              intent.putExtra(ContactsContract.Intents.Insert.EMAIL, contact.getString("emailWork"));
-            } else if (email_count == 1) {
-              intent.putExtra(ContactsContract.Intents.Insert.SECONDARY_EMAIL_TYPE, ContactsContract.CommonDataKinds.Email.TYPE_WORK);
-              intent.putExtra(ContactsContract.Intents.Insert.SECONDARY_EMAIL, contact.getString("emailWork"));
-            }
-          }
         }
-
-        
-        intent.putExtra("finishActivityOnSaveCompleted", true);
-        cordova.startActivityForResult(this, intent, INSERT_CONTACT);
-
-        PluginResult r = new PluginResult(PluginResult.Status.NO_RESULT);
-        r.setKeepCallback(true);
-        callbackContext.sendPluginResult(r);
       } catch (Exception ex) {
         PluginResult r = new PluginResult(PluginResult.Status.ERROR);
         callbackContext.sendPluginResult(r);
@@ -117,10 +73,105 @@ public class ContactCreatorPlugin extends CordovaPlugin {
 
   @Override
   public void onActivityResult(int requestCode, int resultCode, Intent data) {
-    if (resultCode != Activity.RESULT_OK) {
-      callbackContext.error("Parsing contact failed: " + resultCode);
-    } else {
-      callbackContext.success("saved");
+
+    if(resultCode == RESULT_OK){
+      try {
+
+        String accountType = data.getStringExtra(AccountManager.KEY_ACCOUNT_TYPE);
+        String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+
+
+        String displayName = contact.optString("displayName");
+        String phoneWork = contact.has("phoneWork") ? contact.getString("phoneWork") : null;
+        String mobileWork = contact.has("mobileWork") ? contact.getString("mobileWork") : null;
+        String emailWork = contact.has("emailWork") ? contact.getString("emailWork") : null;
+        String street = contact.has("street") ? contact.getString("street") : null;
+        String postCode = contact.has("postCode") ? contact.getString("postCode") : null;
+        String city = contact.has("city") ? contact.getString("city") : null;
+        String organization = contact.has("organization") ? contact.getString("organization") : null;
+        String jobTitle = contact.has("jobTitle") ? contact.getString("jobTitle") : null;
+
+        ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>();
+
+        int rawContactInsertIndex = ops.size();
+        ops.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, accountType)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, accountName)
+                .build());
+
+        ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, rawContactInsertIndex)
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, displayName)
+                .build());
+
+        if(mobileWork != null){
+              ops.add(ContentProviderOperation.
+                  newInsert(ContactsContract.Data.CONTENT_URI)
+                  .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, rawContactInsertIndex)
+                  .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                  .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, mobileWork)
+                  .withValue(ContactsContract.CommonDataKinds.Phone.TYPE,  ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)
+                  .build());
+        }
+
+        if (phoneWork != null) {
+          ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                  .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, rawContactInsertIndex)
+                  .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                  .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, phoneWork)
+                  .withValue(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_WORK)
+                  .build());
+        }
+
+        if (emailWork != null) {
+          ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                  .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, rawContactInsertIndex)
+                  .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)
+                  .withValue(ContactsContract.CommonDataKinds.Email.DATA, emailWork)
+                  .withValue(ContactsContract.CommonDataKinds.Email.TYPE, ContactsContract.CommonDataKinds.Email.TYPE_WORK)
+                  .build());
+        }
+
+        if (organization != null ) {
+          ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                  .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, rawContactInsertIndex)
+                  .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Organization.CONTENT_ITEM_TYPE)
+                  .withValue(ContactsContract.CommonDataKinds.Organization.COMPANY, organization)
+                  .withValue(ContactsContract.CommonDataKinds.Organization.TYPE, ContactsContract.CommonDataKinds.Organization.TYPE_WORK)
+                  .build());
+        }
+
+        if (jobTitle != null ) {
+          ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                  .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, rawContactInsertIndex)
+                  .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Organization.CONTENT_ITEM_TYPE)
+                  .withValue(ContactsContract.CommonDataKinds.Organization.TITLE, jobTitle)
+                  .withValue(ContactsContract.CommonDataKinds.Organization.TYPE, ContactsContract.CommonDataKinds.Organization.TYPE_WORK)
+                  .build());
+        }
+
+        ops.add(ContentProviderOperation
+                .newInsert(ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, rawContactInsertIndex)
+
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_ITEM_TYPE )
+                .withValue(ContactsContract.CommonDataKinds.StructuredPostal.STREET, street)
+
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_ITEM_TYPE )
+                .withValue(ContactsContract.CommonDataKinds.StructuredPostal.CITY, city)
+
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_ITEM_TYPE )
+                .withValue(ContactsContract.CommonDataKinds.StructuredPostal.POSTCODE, postCode)
+
+                .build());
+
+        context.getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
+        callbackContext.success("saved");
+      }
+      catch(Exception ex){
+        callbackContext.error("failed");
+      }
     }
   }
 }
